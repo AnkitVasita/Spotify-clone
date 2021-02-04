e7a4665dcc569494f29e820c5afa53bea126a758
Ankit Vasita <vasitaankit97@gmail.com>
2021-02-04 11:11:58 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCUcWPtc-XEuNgzGiXeAc1aFIl7JUsiiAE",
+  authDomain: "spotify-clone-e5c48.firebaseapp.com",
+  projectId: "spotify-clone-e5c48",
+  storageBucket: "spotify-clone-e5c48.appspot.com",
+  messagingSenderId: "174389180821",
+  appId: "1:174389180821:web:52cfeb4f44efcc28301e9c",
+  measurementId: "G-RK8934F6VZ",
+});
+
+export default firebase;
